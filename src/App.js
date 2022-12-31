4a2b9a26713d1bf26ae04d2ccfab3e8161fa25f4
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-12-30 21:04:58 -0500

    Change App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './App.css'
-
+import {hot} from 'react-hot-loader'
 const App = () => { 
     <div className="App">
         <h1>Hello World!</h1>
     </div>
 }
 
-export default App;
+export default hot(module)(App);
