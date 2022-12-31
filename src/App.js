7dec1bcf81f6a957e928d230ff4efd5edaad3b60
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-12-30 21:21:59 -0500

    Fixed blank page using parens instead of curly braces

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './App.css'
 import {hot} from 'react-hot-loader'
-const App = () => { 
+const App = () => (
     <div className="App">
         <h1>Hello World!</h1>
     </div>
-}
+);
 
 export default hot(module)(App);
