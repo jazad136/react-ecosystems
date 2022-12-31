188be4f95a4b3519bb326acf757c343d19e3e62b
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-12-31 16:13:20 -0500

    Add NewTodoForm

---
+++ src/todos/NewTodoForm.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const NewTodoForm = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <div className="new-todo-form">
+      <input 
+        className="new-todo-input" 
+        type="text"
+        placeholder="Type your new todo here"
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+      />
+      <button className="new-todo-button">Create Todo</button>
+    </div>
+  );
+};
+export default NewTodoForm;
