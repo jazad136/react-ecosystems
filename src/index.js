0497d93ef01a46184830fd4f466915d645d42dfe
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-12-30 21:22:12 -0500

    Use React Strict Mode to catch future errors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 import App from './App.js'
 
-const el = document.getElementById('root')
-const root = ReactDOM.createRoot(el)
-root.render(<App />)
+const rootElement = document.getElementById('root')
+const root = ReactDOM.createRoot(rootElement)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
 // ReactDOM.render(<App />, document.getElementById('root'))
