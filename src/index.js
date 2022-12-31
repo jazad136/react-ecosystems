dee5c4d3d752e37ebf00d88da8a6952869940b86
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-12-30 21:04:04 -0500

    Finish WebpackConfigJS configuration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App.js'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const el = document.getElementById('root')
+const root = ReactDOM.createRoot(el)
+root.render(<App />)
+// ReactDOM.render(<App />, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,34 @@ const webpack = require('webpack')
 module.exports = {
     entry: './src/index.js',
     mode: 'development',
-    
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                options: { presets: ["@babel/env"] }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+        ]
+    },
+    resolve: { extensions: ['*', '.js', '.jsx'] },
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public/'),
+        },
+        port: 3000,
+        devMiddleware: {
+            publicPath: 'http://localhost:3000/dist/'
+        },
+        hot: 'only'
+    }
 }
